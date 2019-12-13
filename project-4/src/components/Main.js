6d56a9de1891fe50ad1572de844f7061255d2f0e
James Bennett <jamesbennett@Jamess-MacBook-Air.local>
2019-12-13 16:43:20 -0600

    created components

---
+++ project-4/src/components/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Build from './Build.js'
+import Form from './Form.js'
+
+class Main extends React.Component {
+
+
+    render(){
+
+        
+        return(
+
+        )
+    }
+}
+
+
+
+
+export default Main
