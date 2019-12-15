e7b5ae38a2e09d40f1945c3659dbc4a15d53bb24
James Bennett <jamesbennett@Jamess-MacBook-Air.local>
2019-12-15 12:01:03 -0600

    made some changes to main.js

--- src/App.js
+++ src/App.js
@@ -91,31 +91,33 @@ class App extends React.Component {
             <Main
             view={this.state.view}
             handleView = {this.handleView}
-            formInputs {this.state.formInputs}
+            // formInputs {this.state.formInputs}
             />
             </div>
         )
     }
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
+
+// function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+// );
+// }
+// }
 
 export default App;
