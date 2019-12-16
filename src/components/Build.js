42cf7666bd5fd35f9e7f68af7cf4631ed6283292
James Bennett <jamesbennett@Jamess-MacBook-Air.local>
2019-12-16 13:50:36 -0600

    commit changes to show builds

--- src/components/Build.js
+++ src/components/Build.js
@@ -4,7 +4,21 @@ import Form from './Form.js'
 class Build extends React.Component {
     render (){
         return (
+            <article>
             <Form />
+            <div className="build-header">
+            <h1>{this.props.buildData.name}</h1>
+            </div>
+            <div className="build-body">
+            {this.props.buildData}
+            </div>
+            <div className="build-options">
+            <ul>
+            <li onClick={() => {this.props.handleView('editBuild', this.props.postData)}}>edit build</li>
+            <li onClick={() => {this.props.handleDelete(this.props.postData.id)}}>Delete Build</li>
+            </ul>
+            </div>
+            </article>
         )
     }
 }
