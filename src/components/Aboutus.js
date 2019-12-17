e40b66b9c6093eca31ba9f22d34a6d78802c8c42
Derek <derekdavis408@gmail.com>
2019-12-17 11:33:20 -0800

    added component  aboutus

---
+++ src/components/Aboutus.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Aboutus extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Derek Davis</h1>
+                <h1>James Bennett</h1>
+            </div>
+        )
+    }
+}
+
+export default Aboutus
