175f61cea87a1df2fca9011c571e42410b2a2148
James Bennett <jamesbennett@Jamess-MacBook-Air.local>
2019-12-17 10:25:16 -0600

    build directory

---
+++ build/precache-manifest.79130a32f173e1164bba3f9873cdabd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1775bed38dac99128f0b081a404fd119",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86fb5dd1091347ed346e",
+    "url": "/static/css/main.8c25f60b.chunk.css"
+  },
+  {
+    "revision": "22068b9d32cf31b9332d",
+    "url": "/static/js/2.64b15e31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.64b15e31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86fb5dd1091347ed346e",
+    "url": "/static/js/main.6eb5f6b5.chunk.js"
+  },
+  {
+    "revision": "8a734c6826f7f1b46684",
+    "url": "/static/js/runtime-main.af017223.js"
+  }
+]);
